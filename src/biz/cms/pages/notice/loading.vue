97c1bd360ff441d23e86cf8d78e7f82670fb0b69
huanglijun <huanglijun317@gmail.com>
2020-11-03 10:40:22 +0800

    "文件夹区分大小写"

---
+++ src/biz/cms/pages/notice/loading.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="page-loading"
+       class="page-loading">
+    <el-table v-loading="loading"
+              :data="tableData"
+              style="width: 100%">
+      <el-table-column prop="date"
+                       label="日期"
+                       width="180">
+      </el-table-column>
+      <el-table-column prop="name"
+                       label="姓名"
+                       width="180">
+      </el-table-column>
+      <el-table-column prop="address"
+                       label="地址">
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<style>
+.page-loading {
+  width: 750px;
+  padding: 25px;
+}
+</style>
+
+<script>
+export default {
+  data () {
+    return {
+      tableData: [{
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }],
+      loading: true
+    }
+  }
+};
+</script>
